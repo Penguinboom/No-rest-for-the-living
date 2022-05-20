17ed5558dff2b692d8398c0211b60ae4c4658edc
Penguinboom <77759407+Penguinboom@users.noreply.github.com>
2022-05-20 14:40:15 -0600

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,19 @@
+import time
+print("To start the story use must know who you are?")
+time.sleep(1)
+input("First off what is your gender?")
+gender = input
+time.sleep(1)
+input("Next, choose a specality: 1.Recon,2.Assault,3.Marksman,4.Combat Medic,5.Heavy,6.Demo-man,7.Recon,8.Tech Specalist.")
+speciality = input
+time.sleep(1)
+input("Finally, What is your name?")
+name = input
+time.sleep(1)
+print("You joined the reserves for the free college... not to fight in a brutal war.")
+time.sleep(4)
+print("You received basic training and conditioning, not too bad considering it was the military.")
+time.sleep(2)
+print("two years later you took an odd test. You were put in a room that emniated an ominus glow, but it might of just been the lighting, you were given modest acommadtions, you stayed there for around 12 hours.")
+time.sleep(1)
+print("A Reasercher came up to you and said ''Your test results said you have no gift. Choose what class you want to specailize in?", "You ended up choosing", speciality)
